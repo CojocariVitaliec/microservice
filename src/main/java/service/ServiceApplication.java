690c21330c49cc57243ffe285b5ed9833b8a9aa4
Vitalie Cojocari <viatlie.cojocari@codefactorygrup.com>
2019-04-12 16:21:16 +0300

    added fix

--- src/main/java/service/ServiceApplication.java
+++ src/main/java/service/ServiceApplication.java
@@ -3,9 +3,14 @@ package service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class ServiceApplication {
     public static void main(String[] args){
-       SpringApplication.run(ServiceApplication.class,args);
-    }
-}
+    SpringApplication app = new SpringApplication(ServiceApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
+        app.run(args);
+
+
+}}
